e4cf4a99bbe21335418a127a143fe2b51cc60eb2
rakeshdas1 <rakeshdaskowshik@gmail.com>
2015-08-10 17:01:23 -0500

    fixed email

--- js/main.js
+++ js/main.js
@@ -195,7 +195,9 @@ jQuery(function($) {'use strict';
 		event.preventDefault();
 		var form_status = $('<div class="form_status"></div>');
 		$.ajax({
+            type: "POST",
 			url: $(this).attr('action'),
+            data: form.serialize(),
 			beforeSend: function(){
 				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
 			}
--- sendemail.php
+++ sendemail.php
@@ -1,18 +1,14 @@
 <?php
-$name       = @trim(stripslashes($_POST['name'])); 
-$from       = @trim(stripslashes($_POST['email'])); 
-$subject    = @trim(stripslashes($_POST['subject'])); 
-$message    = @trim(stripslashes($_POST['message'])); 
+$name       = trim(stripslashes($_POST['name']));
+$from       = trim(stripslashes($_POST['email']));
+$subject    = trim(stripslashes($_POST['subject']));
+$message    = trim(stripslashes($_POST['message']));
 $to   		= 'rakesh@rakeshdas.com';//replace with your email
 
-$headers   = array();
-$headers[] = "MIME-Version: 1.0";
-$headers[] = "Content-type: text/plain; charset=iso-8859-1";
-$headers[] = "From: {$name} <{$from}>";
-$headers[] = "Reply-To: <{$from}>";
-$headers[] = "Subject: {$subject}";
-$headers[] = "X-Mailer: PHP/".phpversion();
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+$headers .= "From: {$name} <{$from}>\r\n";
+$headers .= "Reply-To: <{$from}>\r\n";
+$headers .= "Subject: {$subject}\r\n";
 
 mail($to, $subject, $message, $headers);
-
-die;
